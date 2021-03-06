2516a830ccd874aa69932e8bf3b3593fb726de61
Francisco Ventura <a173379@une.edu.mx>
2021-03-05 19:13:56 -0600

    Listo el generador de variables * Pendientes mejoras en tomar variables
    Co-authored-by: HectorJ2810 <HectorJ2810@users.noreply.github.com>

--- var_generator.py
+++ var_generator.py
@@ -1,42 +1,40 @@
+# http://compoasso.free.fr/primelistweb/page/prime/liste_online_en.php
+# def select_next_variables(n):
+#     prime_values = [6133, 6143, 6151, 6163, 6173]
+#     c = ((2 + n % 5) * 200) + 21
+#     a = 10 ** (n % 2 + 2) + 1
+#     return {'a': a, 'c': c, 'm': prime_values[n], 'seed': 1}
+
+
 def select_next_variables(n):
-    prime_values = [6121, 6131, 6133, 6143, 6151, 6163, 6173, 6197, 6199, 6203, 6211]
-    c = ((2 + n % 5) * 200) + 21
-    a = 10 ** (n % 2 + 2) + 1
-    return {'a': a, 'c': c, 'm': prime_values[n], 'seed': n % 3}
+    a = n + n % 10
+    if a % 2 != 1:
+        a += 1
+    c = 421
+    return {'a': a, 'c': c, 'm': 1039, 'seed': 0}
 
 
 def check_full_period(a, c, m, seed):
-    list_of_Xn1 = []
-    list_of_Xn1.append((a*seed+c) % m)
-    for num in range(m - 1):
-        Xn1 = (a*list_of_Xn1[num]+c) % m
-        if Xn1 in list_of_Xn1:
-            return False, num + 1
-        list_of_Xn1.append(Xn1)
+    list_of_Xn = [seed]
+    for num in range(m - 2):
+        Xn1 = (a*list_of_Xn[num]+c) % m
+        if Xn1 in list_of_Xn:
+            return False, num
+        list_of_Xn.append(Xn1)
+    # f = open("output.txt", "a")
+    # f.write(str(list_of_Xn) + '\n')
+    # f.close()
     return True
 
 
-# for n in range(10):
-#     values = select_next_variables(n)
-#     print(check_full_period(values['a'],
-#                             values['c'],
-#                             values['m'],
-#                             values['seed']),
-#                             values)
+for n in range(500):
+    values = select_next_variables(n)
+    print(check_full_period(values['a'],
+                            values['c'],
+                            values['m'],
+                            values['seed']),
+          values)
 
-print(check_full_period(5,7,8,4))
-print(check_full_period(5,7,16,5))
-print(check_full_period(5,7,11,5))
-
-#                       Resultados
-#      (Serie completa / valor de error) / combinación
-# (False, 2039) {'a': 101, 'c': 421, 'm': 6121, 'seed': 102}
-# (False, 3064) {'a': 1001, 'c': 621, 'm': 6131, 'seed': 102}
-# (False, 6131) {'a': 101, 'c': 821, 'm': 6133, 'seed': 102}
-# (False, 6141) {'a': 1001, 'c': 1021, 'm': 6143, 'seed': 102}
-# (False, 6149) {'a': 101, 'c': 1221, 'm': 6151, 'seed': 102}
-# (False, 6161) {'a': 1001, 'c': 421, 'm': 6163, 'seed': 102}
-# (False, 6171) {'a': 101, 'c': 621, 'm': 6173, 'seed': 102}
-# (False, 3097) {'a': 1001, 'c': 821, 'm': 6197, 'seed': 102}
-# (False, 2065) {'a': 101, 'c': 1021, 'm': 6199, 'seed': 102}
-# (False, 3100) {'a': 1001, 'c': 1221, 'm': 6203, 'seed': 102}
+# print(check_full_period(5, 7, 8, 4))
+# print(check_full_period(5, 7, 16, 5))
+# print(check_full_period(5, 7, 11, 5))
