bc114cd109deacc955a93c5494c218e5d5d247f8
PacoV3 <franj.vents@gmail.com>
2021-03-16 23:49:51 -0600

    Montecarlo PI final
    Co-authored-by: HectorJ2810 <HectorJ2810@users.noreply.github.com>

--- mixto.py
+++ mixto.py
@@ -2,7 +2,7 @@ from ast import literal_eval as make_tuple
 from new_line_txt import write_line_txt
 from var_generator import generate_values
 from test import check_mean_test, check_frequency_test, check_series_test, check_poker_test
-from rand_tools import uniform
+from rand_tools import uniform, pi_montecarlo
 
 
 def select_next_variables(file_name):
@@ -33,6 +33,10 @@ def rand_mix(reset_vars=False, from_txt='txts/variables'):
     rand_mix.count += 1
     # Si la cuenta es igual a m -> borra para generar nuevos números
     if rand_mix.count == rand_mix.m:
+        with open(from_txt + '.txt', 'r') as f:
+            lines = f.readlines()
+        with open(from_txt + '.txt', 'w') as f:
+            f.writelines(lines[1:])
         del rand_mix.count
     # Regresa la variable dividiendo Xn + 1 entre m
     return Xn1 / rand_mix.m
@@ -131,10 +135,10 @@ def use_poker_test(sample_size):
 
 
 if __name__ == "__main__":
-    # initial_variables = 50
+    # initial_variables = 500
     # generate_values(initial_variables, 'txts/variables')
 
-    # sample_size = 100
+    # sample_size = 200
     # # Accuracy:  0.9500, Errors: 5, Sample size: 100, Turns: 100
     # acc, errors, turns = use_mean_test(sample_size)
     # print(f"Mean Test Results - {turns - errors} pass from {initial_variables}\nAccuracy: {acc:6.4f}, Errors: {errors}, Sample size: {sample_size}, Turns: {turns}\n")
@@ -156,8 +160,10 @@ if __name__ == "__main__":
     # acc, errors, turns = use_poker_test(sample_size)
     # print(f"Poker Test Results - {turns - errors} pass from {initial_variables}\nAccuracy: {acc:6.4f}, Errors: {errors}, Sample size: {sample_size}, Turns: {turns}\n")
 
-    for times in range(20):
-        rand_val = rand_mix(from_txt='txts/poker_variables')
-        print(rand_val,uniform(-1, 1, rand_val))
-        # if times % 1277 == 0:
-        #     print(rand_val)
+    r0_r1_list = []
+    for times in range(100000):
+        r0 = rand_mix(from_txt='txts/poker_variables')
+        r1 = rand_mix(from_txt='txts/poker_variables')
+        r0_r1_list.append((r0, r1))
+        if times % 10000 == 0 and times != 0:
+            print(pi_montecarlo(r0_r1_list), len(r0_r1_list))
--- rand_tools.py
+++ rand_tools.py
@@ -5,4 +5,11 @@ def uniform(a, b, R):
 # list_of_pair_rands = [ (R0,R1), (R0,R1), (R0,R1) ]
 def pi_montecarlo(list_of_pair_rands):
     n = len(list_of_pair_rands)
-    
+    n_inside_circle = 0
+    for r0, r1 in list_of_pair_rands:
+        x = uniform(-1, 1, r0)
+        y = uniform(-1, 1, r1)
+        d = x ** 2 + y ** 2
+        if d < 1:
+            n_inside_circle += 1
+    return n_inside_circle / n * 4
